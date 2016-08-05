34001e3b1ccf2916f134dc986790524688152766
dhr9 <dhr.reshamwala@gmail.com>
2016-08-05 20:15:55 +0530

    Added content related to First Video Tutorial

--- app/src/main/java/dhr/emergencytexttutorial/CommonEmergencies.java
+++ app/src/main/java/dhr/emergencytexttutorial/CommonEmergencies.java
@@ -2,6 +2,9 @@ package dhr.emergencytexttutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class CommonEmergencies extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class CommonEmergencies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_emergencies);
+    }
+
+    public void sendText(View view)
+    {
+//        Toast.makeText(this,"Test",Toast.LENGTH_SHORT).show();
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage("6505551212", null, "Bachaooo", null, null);
     }
 }
